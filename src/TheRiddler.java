a7a64bd8eb23737341cbeb83f89d45a3c90b5ee8
Ryder <league@wts22.attlocal.net>
2018-02-20 16:53:33 -0800

    Module 1.

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,24 +1,39 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
-
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		String answer = JOptionPane.showInputDialog("What do other people use more than you but it is yours?");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by
+		// one
+		if (answer.equals("my name")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong! the answer is Your Name.");
+		}
 		// 6. Add some more riddles
+		String answer2 = JOptionPane
+				.showInputDialog("Poor people have it. Rich people need it. If you eat it you die. What is it?");
 
+		if (answer2.equals("nothing")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong! The answer is 'nothing'.");
+		}
 		// 2. Make a pop up to show the score.
-		
+		JOptionPane.showMessageDialog(null, "Your score is " + score);
 	}
 }
-
